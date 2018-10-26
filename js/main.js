816f7a2b2d55ed30b0a0c6a5187e6d6eaf713e8a
David <davidchincharashvili@gmail.com>
2018-10-26 13:44:18 +0400

    with aproximate answers

--- js/main.js
+++ js/main.js
@@ -9,7 +9,10 @@ global.vm = new Vue({
         arr: new Array(),
         show: false,
         answer: false,
-        one: new Array().rand(10,99)
+        one: new Array().rand(10,99),
+        tests: [],
+        weakNums: [],
+        rightClass: {good:false}
     },
     computed:{
         number: function () {
@@ -33,7 +36,41 @@ global.vm = new Vue({
         },
 
         next: function () {
-           this.one = this.addZero(this.arr.rand(0,99));
+            this.answer = false;
+            this.one = this.addZero(this.arr.rand(0,99));
+            this.testQuestions(8);
+        },
+        testQuestions: function (num) {
+            let This = this;
+            let returnValue = [];
+
+            this.arr.loop({to:num}, function (i) {
+                let oneTestQ = This.addZero( This.arr.rand(0,99) );
+                    returnValue.push( oneTestQ );
+            });
+
+            let thRand = This.arr.rand(0,num+1);
+
+            returnValue[thRand] = this.one;
+
+            this.tests= returnValue;
+
+            console.log(this.tests);
+            return returnValue;
+        },
+        isRight(e,bool){
+            "use strict";
+            console.log(e,bool);
+            let trueAnswer = e.target.src.match(this.one);
+            if(trueAnswer){
+                this.rightClass = {good: bool===this.one};
+                setTimeout(()=> {
+                    this.next();
+                    this.rightClass = {good: false}
+                },1000);
+            } else {
+                this.weakNums.push(this.one);
+            }
         }
     }
 
